c4ed1f8b36737bdfd75626f37e9b5694dafa3fec
Suleyman Meral <mrlslymn02@gmail.com>
2026-04-23 01:30:41 +0300

    feat: add MeetingRepository, IMeetingRepository,MeetingConfiguration

--- ShepidiSoftAcademy/src/infrastructure/ShepidiSoft.Persistence/Meetings/MeetingConfiguration.cs
+++ ShepidiSoftAcademy/src/infrastructure/ShepidiSoft.Persistence/Meetings/MeetingConfiguration.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using ShepidiSoft.Domain.Entities;
 
 public class MeetingConfiguration : IEntityTypeConfiguration<Meeting>
 {
---
+++ ShepidiSoftAcademy/src/infrastructure/ShepidiSoft.Persistence/Meetings/MeetingRepository.cs
@@ -0,0 +1,8 @@
+﻿using ShepidiSoft.Application.Contracts.Persistence;
+using ShepidiSoft.Persistence.Context;
+
+namespace ShepidiSoft.Persistence.Meetings;
+
+public sealed class MeetingRepository(AppDbContext context) : GenericRepository<Meeting, int>(context), IMeetingRepository
+{
+}
