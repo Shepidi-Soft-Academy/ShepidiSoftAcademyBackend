64fdcf7cf9156af18085e542a516771a2178894b
Suleyman Meral <mrlslymn02@gmail.com>
2026-05-04 20:18:41 +0300

    feat(application-api): DeleteCommunityService Feature

--- ShepidiSoftAcademy/src/api/ShepidiSoft.API/Controllers/CommunityServicesController.cs
+++ ShepidiSoftAcademy/src/api/ShepidiSoft.API/Controllers/CommunityServicesController.cs
@@ -1,18 +1,29 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ShepidiSoft.API.Abstraction;
+using ShepidiSoft.Application.Features.Activities.Commands.DeleteActivity;
 using ShepidiSoft.Application.Features.CommunityServices.Commands.CreateCommunityService;
+using ShepidiSoft.Application.Features.CommunityServices.Commands.DeleteCommunityService;
 
 namespace ShepidiSoft.API.Controllers;
 
 
 public class CommunityServicesController(IMediator mediator) : BaseApiController(mediator)
 {
+    [HttpPost]
     public async Task<IActionResult> Create(
-CreateCommunityServiceCommand request,
-CancellationToken cancellationToken)
-=> CreateActionResult(await _mediator.Send(request, cancellationToken));
+    CreateCommunityServiceCommand request,
+    CancellationToken cancellationToken)
+    => CreateActionResult(await _mediator.Send(request, cancellationToken));
 
 
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
+    {
+        var command = new DeleteCommunityServiceCommand(id);
+
+        return CreateActionResult(
+            await _mediator.Send(command, cancellationToken));
+    }
 
 }
---
+++ ShepidiSoftAcademy/src/core/ShepidiSoft.Application/Features/CommunityServices/Commands/DeleteCommunityService/DeleteCommunityServiceCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using ShepidiSoft.Application.Contracts.Persistence;
+using System.Net;
+
+namespace ShepidiSoft.Application.Features.CommunityServices.Commands.DeleteCommunityService;
+
+public sealed class DeleteCommunityServiceCommandHandler(
+    ICommunityServiceRepository communityServiceRepository,
+    IUnitOfWork unitOfWork
+    ) : IRequestHandler<DeleteCommunityServiceCommand, ServiceResult>
+{
+    public async Task<ServiceResult> Handle(DeleteCommunityServiceCommand request, CancellationToken cancellationToken)
+    {
+        var communityService =  await communityServiceRepository.GetByIdAsync(request.Id);
+
+        if (communityService is null)
+            return ServiceResult.Fail("Hizmet bulunamadı!", HttpStatusCode.NotFound);
+
+        communityServiceRepository.Delete(communityService);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return ServiceResult.Success(HttpStatusCode.NoContent);
+
+    }
+}
