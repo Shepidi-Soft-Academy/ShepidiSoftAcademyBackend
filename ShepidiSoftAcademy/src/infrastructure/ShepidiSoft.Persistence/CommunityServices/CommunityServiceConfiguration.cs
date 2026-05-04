8e1a881b41c7610bdb1f9821a41c4d0283fd5799
Suleyman Meral <mrlslymn02@gmail.com>
2026-05-04 19:15:04 +0300

    feat(persistence): add CommunityService Configuration

---
+++ ShepidiSoftAcademy/src/infrastructure/ShepidiSoft.Persistence/CommunityServices/CommunityServiceConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShepidiSoft.Domain.Entities;
+
+namespace ShepidiSoft.Persistence.CommunityServices;
+
+public sealed class CommunityServiceConfiguration : IEntityTypeConfiguration<CommunityService>
+{
+    public void Configure(EntityTypeBuilder<CommunityService> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.Description)
+            .IsRequired()
+            .HasMaxLength(2000);
+
+        builder.Property(x => x.IsActive)
+            .IsRequired()
+            .HasDefaultValue(true);
+
+        builder.Property(x => x.ImageUrl)
+            .IsRequired(false)
+            .HasMaxLength(500);
+
+        builder.Property(x => x.Created)
+            .IsRequired();
+
+        builder.Property(x => x.CreatedBy)
+            .IsRequired(false);
+
+        builder.Property(x => x.Updated)
+            .IsRequired(false);
+
+        builder.Property(x => x.UpdatedBy)
+            .IsRequired(false);
+
+        // Index'ler
+        builder.HasIndex(x => x.IsActive);
+        builder.HasIndex(x => x.Created);
+    }
+}
+
