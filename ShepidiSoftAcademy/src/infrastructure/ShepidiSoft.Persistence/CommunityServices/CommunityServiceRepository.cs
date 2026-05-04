98b5b3b6fadcbbe0eb63d1838b65581ceb26de35
Suleyman Meral <mrlslymn02@gmail.com>
2026-05-04 19:19:25 +0300

    feat(persistence-application): add Repo Pattern for CommunityService

--- ShepidiSoftAcademy/src/infrastructure/ShepidiSoft.Persistence/CommunityServices/CommunityServiceRepository.cs
+++ ShepidiSoftAcademy/src/infrastructure/ShepidiSoft.Persistence/CommunityServices/CommunityServiceRepository.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ShepidiSoft.Application.Contracts.Persistence;
+using ShepidiSoft.Domain.Entities;
+using ShepidiSoft.Persistence.Context;
 
-namespace ShepidiSoft.Persistence.CommunityServices
+namespace ShepidiSoft.Persistence.CommunityServices;
+
+public sealed class CommunityServiceRepository(AppDbContext context):GenericRepository<CommunityService, int>(context), ICommunityServiceRepository
 {
-    internal class CommunityServiceRepository
-    {
-    }
 }
 
