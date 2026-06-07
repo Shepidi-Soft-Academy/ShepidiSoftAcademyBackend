c115c30a56af18990ee71e4ab2654c6f00168e67
Suleyman Meral <mrlslymn02@gmail.com>
2026-06-07 04:26:46 +0300

    fix GetDocumentListAdmin feature

--- ShepidiSoftAcademy/src/infrastructure/ShepidiSoft.Persistence/Documents/DocumentRepository.cs
+++ ShepidiSoftAcademy/src/infrastructure/ShepidiSoft.Persistence/Documents/DocumentRepository.cs
@@ -1,9 +1,17 @@
-﻿using ShepidiSoft.Application.Contracts.Persistence;
+﻿using Microsoft.EntityFrameworkCore;
+using ShepidiSoft.Application.Contracts.Persistence;
 using ShepidiSoft.Domain.Entities;
 using ShepidiSoft.Persistence.Context;
 
 namespace ShepidiSoft.Persistence.Documents;
 
-public sealed class DocumentRepository(AppDbContext context) : GenericRepository<Document, int>(context),IDocumentRepository
+public sealed class DocumentRepository(AppDbContext context) : GenericRepository<Document, int>(context), IDocumentRepository
 {
+    public async Task<List<Document>> GetDocumentsWithTopicsAsync()
+    {
+        return await context.Documents
+            .Include(d => d.DocumentTopic)
+            .ToListAsync();
+
+    }
 }
