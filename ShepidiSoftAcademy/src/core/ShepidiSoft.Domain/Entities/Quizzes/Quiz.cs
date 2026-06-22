28865fd7de4be61865588d5a24d6099d7afac24a
Suleyman Meral <mrlslymn02@gmail.com>
2026-06-22 14:20:00 +0300

    feat: add domain entities for Quiz, Question, and QuizCategory

---
+++ ShepidiSoftAcademy/src/core/ShepidiSoft.Domain/Entities/Quizzes/Quiz.cs
@@ -0,0 +1,19 @@
+
+using ShepidiSoft.Domain.Entities.Common;
+using ShepidiSoft.Domain.Entities.Quizzes;
+
+public class Quiz : BaseEntity<int>, IAuditEntity
+{
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public int TimeLimit { get; set; } = default!;
+    public int ScoreLimit { get; set; } = default!;
+    public int CategoryId { get; set; } 
+    public bool IsPublish { get; set; } = false;
+    public QuizCategory? Category { get; set; } 
+    public DateTime Created { get; set; } = default!;
+    public Guid? CreatedBy { get; set; } = default!;
+    public DateTime? Updated { get; set; } = default!;
+    public Guid? UpdatedBy { get; set; } = default!;
+    public ICollection<Question>? Questions { get; set; } = default!;
+}
---
+++ ShepidiSoftAcademy/src/core/ShepidiSoft.Domain/Entities/Quizzes/QuizCategory.cs
@@ -0,0 +1,14 @@
+﻿using ShepidiSoft.Domain.Entities.Common;
+
+namespace ShepidiSoft.Domain.Entities.Quizzes;
+
+public class QuizCategory : BaseEntity<int>, IAuditEntity
+{
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public DateTime Created { get; set; } = default!;
+    public Guid? CreatedBy { get; set; } = default!;
+    public DateTime? Updated { get; set; } = default!;
+    public Guid? UpdatedBy { get; set; } = default!;
+    public ICollection<Quiz>? Quizzes { get; set; }
+}
