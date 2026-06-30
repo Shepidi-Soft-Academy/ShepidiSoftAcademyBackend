e1edcff804bb6c8008908462a413586a619d3a8c
Sultan Karapınar <senin-emailin@example.com>
2026-06-30 11:41:59 +0300

    New URL fields have been added to the Organization entity.

--- ShepidiSoftAcademy/src/core/ShepidiSoft.Domain/Entities/Organizations/Organization.cs
+++ ShepidiSoftAcademy/src/core/ShepidiSoft.Domain/Entities/Organizations/Organization.cs
@@ -7,6 +7,10 @@ public sealed class Organization : BaseEntity<int>, IAuditEntity
     public string Name { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string? LogoUrl { get; set; }
+    public string? LinkedInUrl { get; set; }
+    public string? InstagramUrl { get; set; }
+    public string? WhatsappUrl { get; set; }
+    public string? YoutubeUrl { get; set; }
     public DateTime Created { get; set; }
     public DateTime? Updated { get; set; }
     public Guid? CreatedBy { get; set; }
